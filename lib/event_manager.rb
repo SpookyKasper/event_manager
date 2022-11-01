659df9e52f275c0f20546e514a4103c86d875ffd
Kasper <kasperbak89@pm.me>
2022-11-01 20:14:33 +0100

    Implement a solution for zipcodes (too short, too long or missing)

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -10,5 +10,17 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+  if zipcode.nil?
+    zipcode = '00000'
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  elsif zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+  end
+  # if the zipcode is exactly five digits, assume it's ok
+  # if the zipcode is more than 5 digits truncate until it's 5
+  # if the zipcode is less than 5 digits add 0's until it's 5
+
   puts "This person #{name} lives in #{zipcode}"
 end
