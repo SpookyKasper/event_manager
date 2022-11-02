8e8c8bc767cc56e4192439ff6cb822466e85c3a3
Kasper <kasperbak89@pm.me>
2022-11-02 22:37:54 +0100

    Start implementing Assignment as in TOP 'Clean Phone Numbers'

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,35 @@
 require 'csv'
-puts 'Event Manager Initialized!'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'no legislator found'
+  end
+end
+
+def save_thank_you_letter(id, personal_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  file_name = "output/thank_you_bro_#{id}.html"
+
+  File.open(file_name, 'w') do |file|
+    file.puts personal_letter
+  end
+end
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -7,20 +37,21 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-def clean_zipcode(zipcode)
-  if zipcode.nil?
-    '00000'
-  elsif zipcode.length > 5
-    zipcode[0..4]
-  elsif zipcode.length < 5
-    zipcode.rjust(5, '0')
-  else
-    zipcode
-  end
-end
+puts 'EventMaganerInitialized'
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
+
   zipcode = clean_zipcode(row[:zipcode])
-  puts "This person #{name} lives in #{zipcode}"
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  personal_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, personal_letter)
+
 end
