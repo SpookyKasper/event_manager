84113c6245e542f08bdfeb68eaa6ad26d55301b4
Kasper <kasperbak89@pm.me>
2022-11-03 14:44:38 +0100

    Implement cleaning of phone numbers

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,17 +6,27 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone(phone)
+  phone = phone.tr('^0-9', '')
+  return 'invalid phone number' unless phone.length == 10 || phone.length == 11
+
+  if phone.length == 11
+    phone[0] == '1' ? phone = phone[1..] : 'invalid phone number'
+  end
+  phone
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
-  rescue
+  rescue StandardError
     'no legislator found'
   end
 end
@@ -45,13 +55,12 @@ erb_template = ERB.new template_letter
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
+  phone = clean_phone(row[:homephone])
 
   legislators = legislators_by_zipcode(zipcode)
 
   personal_letter = erb_template.result(binding)
 
   save_thank_you_letter(id, personal_letter)
-
 end
